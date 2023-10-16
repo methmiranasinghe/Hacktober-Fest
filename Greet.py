5f6166dbcf5bad4db3d22dd643c37992878715b5
jayesh <jayeshsingh0803@gmail.com>
2023-10-16 23:23:47 +0530

    fixed issue greeting users

--- Greet.py
+++ Greet.py
@@ -1,7 +1,11 @@
 def greet(name):
     """Function to greet the user."""
-    if name.strip().isalpha():
+    # Check if the input is empty or contains only whitespace
+    if not name.strip():
+        return "Invalid input. The name field can not be empty."
+
+    # Check if the input contains only alphabetic characters
+    if name.isalpha():
         return f"Hello, {name.strip()}!"
     else:
         return "Invalid input. Please enter a valid name."
-
