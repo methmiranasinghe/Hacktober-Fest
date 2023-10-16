9c5cf9f7a225ced6ddba1eaecef1d0281595f162
jayesh <jayeshsingh0803@gmail.com>
2023-10-16 23:16:33 +0530

    Fixed issue filehandling

--- file_handling.py
+++ file_handling.py
@@ -1,11 +1,18 @@
 def greet_users_from_file(file_path):
-    """Function to greet users listed in a file."""
+    # Open the file for reading
     try:
-        with open(file_path, 'r') as file:
-            users = file.readlines()
-            for user in users:
-                print(f"Hello, {user.strip()}!")
+        with open("username.txt", "r") as file:
+        # Read the lines from the file
+            lines = file.readlines()
+
+        # Loop through each line (each username) and greet them
+            for line in lines:
+                username = line.strip()  # Remove any leading/trailing whitespace
+                if username:
+                    print(f"Hello, {username}!")
+
     except FileNotFoundError:
-        print("File not found. Please provide a valid file path.")
+        print("File 'username.txt' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
