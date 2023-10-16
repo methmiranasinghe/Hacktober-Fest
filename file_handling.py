09bc138440512e1218e9bd46a0d1f32b6b257c3f
Pasindu Bandara <72030365+PasinduBandaraa@users.noreply.github.com>
2023-10-16 22:38:32 +0530

    Update file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -9,7 +9,3 @@ def greet_users_from_file(file_path):
         print("File not found. Please provide a valid file path.")
     except Exception as e:
         print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    file_path = input("Enter the path to the file containing user names: ")
-    greet_users_from_file(file_path)
