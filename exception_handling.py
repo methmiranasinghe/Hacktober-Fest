4c4d8177bfb352bfcd492437982ad80c5466f797
Pasindu Bandara <72030365+PasinduBandaraa@users.noreply.github.com>
2023-10-16 22:40:35 +0530

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -1,14 +1,10 @@
 def greet(name):
     """Function to greet the user."""
     try:
-        if name.isalpha():
-            return f"Hello, {name}!"
+        if name.strip().isalpha():
+            return f"Hello, {name.strip()}!"
         else:
             raise ValueError("Invalid input. Please enter a valid name.")
     except ValueError as e:
         return str(e)
 
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    greeting_message = greet(user_name)
-    print(greeting_message)
